1d7cd6818ce35948a7bdf25b9b830621cbe3a2a3
Justin S. Leitgeb <justin@stackbuilders.com>
2012-02-04 10:00:26 -0500

    Move method to StubShell scope

--- lib/stub_shell.rb
+++ lib/stub_shell.rb
@@ -1,26 +1,26 @@
-module StubShell  
-  class << self
-    attr_accessor :current_context
-  end
-  
-  self.current_context = nil
-end
-
 require 'stub_shell/result'
 require 'stub_shell/command'
 require 'stub_shell/shell'
 require 'stub_shell/test_helpers'
 
-def run_command cmd
-  command, StubShell.current_context = StubShell.current_context.execute(cmd)
-  Kernel.send(:`, "#{File.join(File.dirname(__FILE__), '..', 'bin', 'fake_process.sh')} '#{command.result.exitstatus}'")
-  command
+module StubShell  
+  class << self
+    attr_accessor :current_context
+    
+    def run_command cmd
+      command, StubShell.current_context = StubShell.current_context.execute(cmd)
+      Kernel.send(:`, "#{File.join(File.dirname(__FILE__), '..', 'bin', 'fake_process.sh')} '#{command.result.exitstatus}'")
+      command
+    end
+  end
+  
+  self.current_context = nil
 end
 
 def `(cmd)
-  run_command(cmd).result.stdout
+  StubShell.run_command(cmd).result.stdout
 end
 
 def system(cmd)
-  run_command(cmd).result.exitstatus == 0
+  StubShell.run_command(cmd).result.exitstatus == 0
 end
